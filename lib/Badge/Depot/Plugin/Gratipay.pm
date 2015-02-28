c3d7ff2afe64b50dda8ba64016a1c77225499011
Erik Carlsson <info@code301.com>
2015-02-28 18:22:08 +0100

    Init

---
+++ lib/Badge/Depot/Plugin/Gratipay.pm
@@ -0,0 +1,29 @@
+use 5.10.1;
+use strict;
+use warnings;
+
+package Badge::Depot::Plugin::Gratipay;
+
+# VERSION
+# ABSTRACT: Short intro
+
+
+
+1;
+
+
+__END__
+
+=pod
+
+=head1 SYNOPSIS
+
+    use Badge::Depot::Plugin::Gratipay;
+
+=head1 DESCRIPTION
+
+Badge::Depot::Plugin::Gratipay is ...
+
+=head1 SEE ALSO
+
+=cut
---
+++ t/01-basic.t
@@ -0,0 +1,9 @@
+use strict;
+use warnings;
+
+use Test::More;
+use if $ENV{'AUTHOR_TESTING'}, 'Test::Warnings';
+use Badge::Depot::Plugin::Gratipay;
+
+fail('this test is TODO!');
+done_testing;
